a7c20ca1b2e12cdd991028616714e14a14b7eb83
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-29 21:07:31 +0300

    added navigation to all screens

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { NavigationStackScreenComponent } from "react-navigation-stack";
 
-const SignUpScreen = () => {
+const SignUpScreen: NavigationStackScreenComponent = ({ navigation }) => {
   return (
     <View>
       <Text>SignUpScreen SignUpScreen</Text>
+      <Button
+        title="Go to Sign in"
+        onPress={() => navigation.navigate("Signin")}
+      />
+      <Button
+        title="Go to main flow"
+        onPress={() => navigation.navigate("mainFlow")}
+      />
     </View>
   );
 };
--- src/screens/TrackListScreen.tsx
+++ src/screens/TrackListScreen.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { NavigationStackScreenComponent } from "react-navigation-stack";
 
-const TrackListScreen = () => {
+const TrackListScreen: NavigationStackScreenComponent = ({ navigation }) => {
   return (
     <View>
       <Text>TrackListScreen TrackListScreen</Text>
+      <Button
+        onPress={() => navigation.navigate("TrackDetail")}
+        title="Go to track detail"
+      />
     </View>
   );
 };
