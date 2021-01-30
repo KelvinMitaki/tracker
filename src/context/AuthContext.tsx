17d32e436853613f21eaa08b53df8ed616e04c09
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-30 20:30:30 +0300

    added user actions

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -6,8 +6,19 @@ interface DefaultAction {
   type: "";
   payload?: any;
 }
+interface SignupAction {
+  type: "signup";
+  payload: { email: string; password: string };
+}
+interface SigninAction {
+  type: "signin";
+  payload: { email: string; password: string };
+}
+interface SignoutAction {
+  type: "signout";
+}
 
-type Action = DefaultAction;
+type Action = DefaultAction | SignupAction;
 
 const reducer = (state: State, action: Action) => {
   switch (action.type) {
@@ -16,8 +27,16 @@ const reducer = (state: State, action: Action) => {
   }
 };
 
+const signup = (dispatch: React.Dispatch<SignupAction>) => (
+  data: SignupAction["payload"]
+) => {};
+const signin = (dispatch: React.Dispatch<SigninAction>) => (
+  data: SigninAction["payload"]
+) => {};
+const signout = (dispatch: React.Dispatch<SignoutAction>) => () => {};
+
 export const { Context, Provider } = createDataContext(
   reducer,
-  {},
+  { signup, signin, signout },
   { isSignedIn: false }
 );
