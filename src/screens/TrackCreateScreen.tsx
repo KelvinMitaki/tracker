a88e1b1eac00a00bdc5fc652f4bad7c2dde60244
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-31 15:56:31 +0300

    added asking user location permission

--- src/screens/TrackCreateScreen.tsx
+++ src/screens/TrackCreateScreen.tsx
@@ -1,11 +1,36 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { requestPermissionsAsync } from "expo-location";
+import React, { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
+import { Text } from "react-native-elements";
+import { SafeAreaView } from "react-navigation";
+import Map from "../components/Map";
 
 const TrackCreateScreen = () => {
+  const [err, setError] = useState<{ [key: string]: string } | string | null>(
+    null
+  );
+  useEffect(() => {
+    startWatching();
+  }, []);
+  const startWatching = async () => {
+    try {
+      const response = await requestPermissionsAsync();
+      if (!response.granted) {
+        setError("denined");
+      } else {
+        setError(null);
+      }
+      console.log(response);
+    } catch (error) {
+      setError(error);
+    }
+  };
   return (
-    <View>
-      <Text>TrackCreateScreen TrackCreateScreen</Text>
-    </View>
+    <SafeAreaView forceInset={{ top: "always" }}>
+      <Text h2>Create A Track</Text>
+      <Map />
+      {err && <Text>Please enable location services</Text>}
+    </SafeAreaView>
   );
 };
 
