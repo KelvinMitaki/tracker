40fc7e2e74da92fe3404f084d60cacdcb191da89
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-31 21:28:51 +0300

    added populating current location on map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,9 +1,42 @@
-import React from "react";
+import { Accuracy, getCurrentPositionAsync } from "expo-location";
+import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import MapView from "react-native-maps";
 
 const Map = () => {
-  return <MapView style={styles.map} />;
+  const [coords, setCoords] = useState<{ latitude: number; longitude: number }>(
+    { latitude: 0, longitude: 0 }
+  );
+  useEffect(() => {
+    const getLocation = async () => {
+      const location = await getCurrentPositionAsync({
+        accuracy: Accuracy.BestForNavigation
+      });
+      const {
+        coords: { latitude, longitude }
+      } = location;
+      setCoords({ latitude, longitude });
+    };
+    getLocation();
+  }, []);
+  const { latitude, longitude } = coords;
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude,
+        longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      }}
+      region={{
+        latitude,
+        longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      }}
+    />
+  );
 };
 
 export default Map;
